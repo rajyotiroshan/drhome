fe4e60986f19c55929b5884b032ad480477f0d8a
rajan <rajyotiroshan@gmail.com>
2023-04-18 23:33:24 +0530

    login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,35 @@
 import React from "react";
- 
+import { Form } from "antd";
+import { Link } from "react-router-dom";
+
 export default function Login(props) {
-    
-    return (
-        <div>Login</div>
-    )
+  const onFinish = (values) => {
+    console.log(values);
+  };
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Form layout="vertical" className="bg-white p-2" onFinish={onFinish}>
+        <h2 className="my-1 uppercase">Login</h2>
+        <hr />
+
+        <Form.Item name="email" label="Email" className="w-400 ">
+          <input type="email" />
+        </Form.Item>
+        <Form.Item name="password" label="Password" className="w-400 ">
+          <input type="password" />
+        </Form.Item>
+
+        <button className="contained-btn" type="submit">
+          Login
+        </button>
+
+        <div>
+          Don't have an account?
+          <Link to="/register" className="underline">
+            <strong> Sign Up</strong>
+          </Link>
+        </div>
+      </Form>
+    </div>
+  );
 }
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,21 +1,36 @@
 import React from "react";
-import { Form, Button } from "antd";
+import { Form } from "antd";
+import { Link } from "react-router-dom";
 
 export default function Register(props) {
+  const onFinish = (values) => {
+    console.log(values);
+  };
   return (
     <div className="flex justify-center items-center h-screen">
-      <Form layout="vertical" className="bg-white p-2">
-        <Form.Item label="Name" className="w-400 bg-white">
+      <Form layout="vertical" className="bg-white p-2" onFinish={onFinish}>
+        <h2 className="my-1 uppercase">Register</h2>
+        <hr />
+        <Form.Item name="name" label="Name" className=" w-400 ">
           <input type="text" />
         </Form.Item>
-        <Form.Item label="Email" className="w-400 bg-white">
+        <Form.Item name="email" label="Email" className="w-400 ">
           <input type="email" />
         </Form.Item>
-        <Form.Item label="Password" className="w-400 bg-white">
+        <Form.Item name="password" label="Password" className="w-400 ">
           <input type="password" />
         </Form.Item>
 
-        <button className="contained-btn">Register</button>
+        <button className="contained-btn" type="submit">
+          Register
+        </button>
+
+        <div>
+          Already have an account?
+          <Link to="/login" className="underline">
+            <strong> Sign In</strong>
+          </Link>
+        </div>
       </Form>
     </div>
   );
