95aab22b9b5779ce0aa9ca791034ee73fb659385
rajan <rajyotiroshan@gmail.com>
2023-04-18 22:07:05 +0530

    pckg install basic page setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Dr. Home</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
