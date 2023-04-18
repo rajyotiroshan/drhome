d83f4840926298551547db5d805040731604db1c
rajan <rajyotiroshan@gmail.com>
2023-04-18 23:06:47 +0530

    register form

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Form, Button } from "antd";
 
 export default function Register(props) {
-  return <div>Register</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Form layout="vertical" className="bg-white p-2">
+        <Form.Item label="Name" className="w-400 bg-white">
+          <input type="text" />
+        </Form.Item>
+        <Form.Item label="Email" className="w-400 bg-white">
+          <input type="email" />
+        </Form.Item>
+        <Form.Item label="Password" className="w-400 bg-white">
+          <input type="password" />
+        </Form.Item>
+
+        <button className="contained-btn">Register</button>
+      </Form>
+    </div>
+  );
 }
